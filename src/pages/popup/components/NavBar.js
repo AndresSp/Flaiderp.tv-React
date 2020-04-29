7cdbd015b989bd1b613ecdb53e343de36be4819a
AndresSp <andrestunonsp@gmail.com>
2020-04-29 03:29:04 -0500

    Navbar Base

--- src/pages/popup/components/NavBar.js
+++ src/pages/popup/components/NavBar.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Menu, Header, Image, Icon } from 'semantic-ui-react';
 
+const styles = {
+  header: {
+    display: 'flex',
+    margin: 'auto',
+    content: {
+      margin: 'auto'
+    }
+  }
+}
+
 const NavBar = (props) => (
     <Menu fixed='top' borderless>
       <Menu.Item>
-        <Header>
-          <Image size='mini'
-          src='./../../../assets/icon/48.png'
-           rounded />
-          <Header.Content>Flaiderp.tv</Header.Content>
+        <Header size='medium' style={styles.header}>
+          <Image src='./../../../assets/icon/48.png' size='mini' rounded/>
+          <Header.Content style={styles.header.content}>Flaiderp.tv</Header.Content>
         </Header>
       </Menu.Item>
-      <Menu.Item position='right'>
-        <Icon name='ellipsis vertical' />
-      </Menu.Item>
+      <Menu.Menu position='right'>
+        <Menu.Item icon>
+          <Icon link name='bell' size='large'/>
+        </Menu.Item>
+        <Menu.Item icon>
+          <Icon link name='ellipsis vertical' size='large'/>
+        </Menu.Item>
+      </Menu.Menu>
     </Menu>
 )
 
