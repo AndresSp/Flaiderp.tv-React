cda07a9cfaad8d246eb33104aa68ad6c3c066fb1
AndresSp <andrestunonsp@gmail.com>
2020-08-09 06:52:42 -0500

    Include chat

--- src/pages/popup/components/StreamsCarousel/StreamSlide.js
+++ src/pages/popup/components/StreamsCarousel/StreamSlide.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Slide, Image } from 'pure-react-carousel';
-import { Card, Icon, Placeholder, Label, Segment } from 'semantic-ui-react';
+import { Card, Icon, Placeholder, Label, Segment, Embed } from 'semantic-ui-react';
 import TwitchPlayerEmbed from './TwitchPlayerEmbed';
 import { getBrowser } from '../../../../modules/apis/extension';
+import {TwitchChat } from 'react-twitch-embed'
 
 const styles = {}
 
@@ -12,7 +13,10 @@ class StreamSlide extends React.PureComponent {
     
     constructor(props){
         super(props)
-        this.isFirefox = getBrowser() === 'FIREFOX'
+        this.state = {
+            isFirefox: getBrowser() === 'FIREFOX',
+            chat: false
+        };
     }
 
     imgPlaceHolder = () => (
@@ -20,6 +24,8 @@ class StreamSlide extends React.PureComponent {
             <Placeholder.Image className='carousel-image-placeholder' />
         </Placeholder>
     )
+
+    onPlay = () => this.setState({ chat: true })
     
 
     render(){
@@ -31,7 +37,7 @@ class StreamSlide extends React.PureComponent {
                     <Card  fluid>
                         { 
                             showLive ? 
-                            (this.isFirefox? <a href={`https://www.twitch.tv/${channel}`} target='_blank'>
+                            (this.state.isFirefox? <a href={`https://www.twitch.tv/${channel}`} target='_blank'>
                                     <Image 
                                     hasMasterSpinner={false} 
                                     isBgImage={true} 
@@ -42,7 +48,7 @@ class StreamSlide extends React.PureComponent {
                                     className='carousel-image'>
                                             <Icon name='external' inverted size='big' color='grey'/>
                                     </Image>
-                                </a> : <TwitchPlayerEmbed preview={image} channel={channel} />) : 
+                                </a> : <TwitchPlayerEmbed preview={image} channel={channel} onPlay={this.onPlay} />) : 
                             <Image 
                             hasMasterSpinner={false} 
                             isBgImage={true} 
@@ -52,31 +58,56 @@ class StreamSlide extends React.PureComponent {
                             src={image} 
                             className='carousel-image'/>
                         }
-                        <Card.Content className='carousel-content'>
-                            <Card.Header className='carousel-header'>
+                        <Card.Content className={this.state.chat ? 'carousel-content-chat' : 'carousel-content'}>
+                        { this.state.chat ? 
+                        <div className='chat-embed-container'>
+                            <TwitchChat
+                                channel={channel}
+                                id={channel}
+                                theme='dark'
+                                parent={['chromiumapp.org']}
+                                width='100%'
+                                height={300}
+                                scrolling='true'
+                                style={{position: 'relative', marginTop: '-135px'}}
+                                />
+                        </div> : (
+                            <React.Fragment>
+                                <Card.Header className='carousel-header'>
                                 { header } 
                                 <Label 
                                 style={{ marginLeft: '5px' }} 
                                 size='tiny' 
                                 color={showLive ? 'green' : 'grey' } 
                                 horizontal>{showLive ? 'LIVE' : 'OFF'}</Label>
-                            </Card.Header>
-                            <Card.Description className='carousel-description'>
-                                { description }
+                                {/* <Label
+                                size='tiny'
+                                color='purple'>
+                                CHAT
+                                </Label> */}
+                                </Card.Header>
+                                <Card.Description className='carousel-description'>
+                                    { description }
                                 </Card.Description>
+                            </React.Fragment>
+                        )
+                        }
                         </Card.Content>
-                        <Card.Content className='carousel-extra' extra>
-                            {
-                                (socials ? Object.keys(socials).map((social, i) => (
-                                    <a key={i} href={socials[social]} target="_blank">
-                                        {
-                                            social.includes('custom') ? 
-                                            <Icon circular className={`social ${social}`}/> :
-                                            <Icon name={social} circular className={'social'}/>
-                                        }
-                                    </a>)) : '')
-                            }
-                        </Card.Content>
+                        {
+                            this.state.chat ? '' :
+                                <Card.Content className='carousel-extra' extra>
+                                {
+                                    (socials ? Object.keys(socials).map((social, i) => (
+                                        <a key={i} href={socials[social]} target="_blank">
+                                            {
+                                                social.includes('custom') ? 
+                                                <Icon circular className={`social ${social}`}/> :
+                                                <Icon name={social} circular className={'social'}/>
+                                            }
+                                        </a>)) : '')
+                                }
+                                </Card.Content>
+                        }
                     </Card>
                 </div>
             </Slide>
--- src/pages/popup/components/StreamsCarousel/TwitchPlayerEmbed.js
+++ src/pages/popup/components/StreamsCarousel/TwitchPlayerEmbed.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Embed } from 'semantic-ui-react';
 
-const TwitchPlayerEmbed = ({ channel, preview }) => (
+const TwitchPlayerEmbed = ({ channel, preview, onPlay }) => (
     <Embed
     icon='play'
     placeholder={preview}
     url={`https://player.twitch.tv/?channel=${String(channel).toLowerCase()}&autoplay=true&muted=false&parent=${["chromiumapp.org"]}`}
+    onClick={onPlay}
     />
 )
 
